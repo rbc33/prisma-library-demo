047861e34f10be2f297cb34f9d568556fad4a26b
rbc33 <ricardobenthem@gmail.com>
2026-02-26 19:33:38 +0100

    prisma 5.15 working

--- app.js
+++ app.js
@@ -2,9 +2,6 @@
 // https://www.npmjs.com/package/dotenv
 require("dotenv").config();
 
-// ℹ️ Connects to the database
-const prisma = require('./db/index');
-
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -15,11 +12,14 @@ const app = express();
 require("./config")(app);
 
 // 👇 Start handling routes here
+const indexRoutes = require("./routes/index.routes");
+app.use("/api", indexRoutes);
+
 const bookRoutes = require("./routes/book.routes");
 app.use("/api", bookRoutes);
 
-const authorRoutes = require('./routes/author.routes');
-app.use('/api', authorRoutes); 
+const authorRoutes = require("./routes/author.routes");
+app.use("/api", authorRoutes);  
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/author.routes.js
+++ routes/author.routes.js
@@ -1,30 +1,29 @@
-// routes/author.routes.js
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const prisma = require('../db/index');
+const prisma = require("../db/index");
+
 
 //  POST /api/authors  -  Creates a new author
-router.post('/authors', (req, res, next) => {
-  const { firstName, lastName, bio } = req.body;
+router.post("/authors", (req, res, next) => {
+    const { firstName, lastName, bio } = req.body;
 
-  const newAuthor = {
-    firstName,
-    lastName,
-    bio
-  };
+    const newAuthor = {
+        firstName,
+        lastName,
+        bio
+    }
 
-  prisma.author
-    .create({ data: newAuthor })
-    .then(author => {
-      console.log('New author created', author);
-      res.status(201).json(author);
-    })
-    .catch(err => {
-      console.log('Error creating new author', err);
-      res.status(500).json({ message: 'Error creating new author' });
-    });
+    prisma.author.create({ data: newAuthor })
+        .then((author) => {
+            console.log("New author created", author);
+            res.status(201).json(author);
+        })
+        .catch((err) => {
+            console.log("Error creating new author", err);
+            res.status(500).json({ message: "Error creating new author" });
+        });
 });
+
 
 module.exports = router;
